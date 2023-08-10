394714719f8f48a334899d37c4be57773c4e4c5a
ARYAN PANWAR <121965055+chaudharyaryanpanwar@users.noreply.github.com>
2023-08-10 10:57:37 +0530

    some more commits

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-dark text-light'>
+    <h6 className='text-center'>made by aryan panwar</h6>
+    </div>
+  )
+}
+
+export default Footer
--- server.js
+++ server.js
@@ -1,3 +1,26 @@
 const express = require("express");
-const morgan  = rquire
+const morgan  = require("morgan");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const connectDb = require("./config/connectDb");
+
+
+dotenv.config();
+
+connectDb();
+
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+
+app.use("/api/v1/users", require(".routes/userRoute"))
+
+const PORT = 8080 || process.env.PORT
+
+app.listen(PORT, ()=>{
+    console.log(`server is running of port ${PORT}`);
+})
