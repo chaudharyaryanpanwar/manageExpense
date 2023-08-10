ce8f5e672ff6f13d2960712038d47dbe340864a5
ARYAN PANWAR <121965055+chaudharyaryanpanwar@users.noreply.github.com>
2023-08-10 11:07:04 +0530

    fwe more commits

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.use(express.json());
 app.use(cors());
 
 
-app.use("/api/v1/users", require(".routes/userRoute"))
+app.use("/api/v1/users", require("./routes/userRoute"));
+
 
 const PORT = 8080 || process.env.PORT
 
