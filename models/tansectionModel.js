3e79384a51f9105e48ed7121ecac147c765dfd98
ARYAN PANWAR <121965055+chaudharyaryanpanwar@users.noreply.github.com>
2023-08-25 13:25:40 +0530

    transaction erro

--- models/tansectionModel.js
+++ models/tansectionModel.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const transectionSchema = new mongoose.Schema(
   {
-    
+    userid: {
+      type: String,
+      required: true,
+    },
     amount: {
       type: Number,
       required: [true, "amount is required"],
